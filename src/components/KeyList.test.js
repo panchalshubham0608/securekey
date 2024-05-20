bd4e3f027f825a6e1ff25c01a5c3fa8c3fb44639
Shubham Panchal <shubhampanchal9773@gmail.com>
2024-05-20 15:34:17 +0530

    fix lint

--- src/components/KeyList.test.js
+++ src/components/KeyList.test.js
@@ -3,172 +3,146 @@ import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import KeysList from "./KeysList";
 import UserContext from "../context/UserContext";
-import { getPassKeys, deletePassKey } from "../utils/firestoredb";
+import { getPassKeys } from "../utils/firestoredb";
 // import debounce from "../utils/debounce";
 
 // Mocking the dependencies
 jest.mock("../utils/firestoredb", () => ({
-    getPassKeys: jest.fn(),
-    deletePassKey: jest.fn(),
+  getPassKeys: jest.fn(),
+  deletePassKey: jest.fn(),
 }));
-// jest.mock("../utils/debounce", () => jest.fn((fn) => fn));
 
 const renderKeysListWithUserContext = (userContextValue, props = {}) => {
-    return render(
-        <UserContext.Provider value={userContextValue}>
-            <KeysList {...props} />
-        </UserContext.Provider>
-    );
+  return render(
+    <UserContext.Provider value={userContextValue}>
+      <KeysList {...props} />
+    </UserContext.Provider>
+  );
 };
 
 describe("KeysList component", () => {
-    let setUser;
+  let setUser;
 
-    beforeEach(() => {
-        setUser = jest.fn();
+  beforeEach(() => {
+    setUser = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("renders Navbar, search input, and loading spinner", async () => {
+    getPassKeys.mockResolvedValue([
+      { account: "test1", username: "user1" },
+      { account: "test2", username: "user2" },
+    ]);
+    renderKeysListWithUserContext({ setUser });
+
+    expect(screen.getByTestId("navbar")).toBeInTheDocument();
+    expect(screen.getByTestId("search-input")).toBeInTheDocument();
+    expect(screen.getByTestId("loader")).toBeInTheDocument();
+
+    await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
+  });
+
+  test("renders list of keys", async () => {
+    getPassKeys.mockResolvedValue([
+      { account: "test1", username: "user1" },
+      { account: "test2", username: "user2" },
+    ]);
+    renderKeysListWithUserContext({ setUser });
+
+    await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(screen.queryByTestId("loader")).not.toBeInTheDocument());
+
+    expect(screen.getByText("test1")).toBeInTheDocument();
+    expect(screen.getByText("user1")).toBeInTheDocument();
+    expect(screen.getByText("test2")).toBeInTheDocument();
+    expect(screen.getByText("user2")).toBeInTheDocument();
+  });
+
+  test("renders list of keys in order of account and usernames", async () => {
+    getPassKeys.mockResolvedValue([
+      { account: "test2", username: "user1" },
+      { account: "test1", username: "user1" },
+      { account: "test1", username: "user2" },
+    ]);
+    renderKeysListWithUserContext({ setUser });
+
+    await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(screen.queryByTestId("loader")).not.toBeInTheDocument());
+
+    const accounts = screen.getAllByTestId("key-account");
+    const usernames = screen.getAllByTestId("key-username");
+    expect(accounts[0]).toHaveTextContent("test1");
+    expect(usernames[0]).toHaveTextContent("user1");
+    expect(accounts[1]).toHaveTextContent("test1");
+    expect(usernames[1]).toHaveTextContent("user2");
+    expect(accounts[2]).toHaveTextContent("test2");
+    expect(usernames[2]).toHaveTextContent("user1");
+  });
+
+  test("shows correct count of keys", async () => {
+    getPassKeys.mockResolvedValue([
+      { account: "test1", username: "user1" },
+      { account: "test2", username: "user2" },
+    ]);
+    renderKeysListWithUserContext({ setUser });
+
+    await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(screen.queryByTestId("loader")).not.toBeInTheDocument());
+
+    expect(screen.getByTestId("keys-list-count").textContent).toBe("Accounts (2)");
+  });
+
+  test("shows error message on fetch failure", async () => {
+    getPassKeys.mockRejectedValue(new Error("Server down"));
+
+    renderKeysListWithUserContext({ setUser });
+
+    await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(screen.queryByTestId("loader")).not.toBeInTheDocument());
+
+    expect(screen.getByTestId("keys-list-error").textContent).toBe("Server down");
+    expect(screen.queryByTestId("keys-account")).not.toBeInTheDocument();
+  });
+
+  test("filters keys based on search input", async () => {
+    getPassKeys.mockResolvedValue([
+      { account: "test1", username: "user1" },
+      { account: "test2", username: "user2" },
+      { account: "prefixedtest1", username: "user3" }
+    ]);
+    renderKeysListWithUserContext({ setUser });
+
+    await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(screen.queryByTestId("loader")).not.toBeInTheDocument());
+
+    fireEvent.change(screen.getByTestId("search-input"), { target: { value: "test1" } });
+
+    await waitFor(() => {
+      expect(screen.getByText("test1")).toBeInTheDocument();
+      expect(screen.queryByText("prefixedtest1")).toBeInTheDocument();
+      expect(screen.queryByText("test2")).not.toBeInTheDocument();
     });
+  });
 
-    afterEach(() => {
-        jest.clearAllMocks();
+  test("shows empty message when no keys are found", async () => {
+    getPassKeys.mockResolvedValue([
+      { account: "test1", username: "user1" },
+      { account: "test2", username: "user2" },
+    ]);
+    renderKeysListWithUserContext({ setUser });
+
+    await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(screen.queryByTestId("loader")).not.toBeInTheDocument());
+
+    fireEvent.change(screen.getByTestId("search-input"), { target: { value: "test3" } });
+
+    await waitFor(() => {
+      let noKeysElement = screen.getByTestId("keys-list-no-keys");
+      expect(noKeysElement).toBeInTheDocument();
+      expect(noKeysElement.textContent).toBe("No keys found");
     });
-
-    test("renders Navbar, search input, and loading spinner", async () => {
-        getPassKeys.mockResolvedValue([
-            { account: "test1", username: "user1" },
-            { account: "test2", username: "user2" },
-        ]);
-        renderKeysListWithUserContext({ setUser });
-
-        expect(screen.getByTestId("navbar")).toBeInTheDocument();
-        expect(screen.getByTestId("search-input")).toBeInTheDocument();
-        expect(screen.getByTestId("loader")).toBeInTheDocument();
-
-        await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
-    });
-
-    test("renders list of keys", async () => {
-        getPassKeys.mockResolvedValue([
-            { account: "test1", username: "user1" },
-            { account: "test2", username: "user2" },
-        ]);
-        renderKeysListWithUserContext({ setUser });
-
-        await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
-        await waitFor(() => expect(screen.queryByTestId("loader")).not.toBeInTheDocument());
-
-        expect(screen.getByText("test1")).toBeInTheDocument();
-        expect(screen.getByText("user1")).toBeInTheDocument();
-        expect(screen.getByText("test2")).toBeInTheDocument();
-        expect(screen.getByText("user2")).toBeInTheDocument();
-    });
-
-    test("renders list of keys in order of account and usernames", async () => {
-        getPassKeys.mockResolvedValue([
-            { account: "test2", username: "user1" },
-            { account: "test1", username: "user1" },
-            { account: "test1", username: "user2" },
-        ]);
-        renderKeysListWithUserContext({ setUser });
-
-        await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
-        await waitFor(() => expect(screen.queryByTestId("loader")).not.toBeInTheDocument());
-
-        const accounts = screen.getAllByTestId("key-account");
-        const usernames = screen.getAllByTestId("key-username");
-        expect(accounts[0]).toHaveTextContent("test1");
-        expect(usernames[0]).toHaveTextContent("user1");
-        expect(accounts[1]).toHaveTextContent("test1");
-        expect(usernames[1]).toHaveTextContent("user2");
-        expect(accounts[2]).toHaveTextContent("test2");
-        expect(usernames[2]).toHaveTextContent("user1");
-    });
-
-    test("shows correct count of keys", async () => {
-        getPassKeys.mockResolvedValue([
-            { account: "test1", username: "user1" },
-            { account: "test2", username: "user2" },
-        ]);
-        renderKeysListWithUserContext({ setUser });
-
-        await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
-        await waitFor(() => expect(screen.queryByTestId("loader")).not.toBeInTheDocument());
-
-        expect(screen.getByTestId("keys-list-count").textContent).toBe("Accounts (2)");
-    });
-
-    test("shows error message on fetch failure", async () => {
-        getPassKeys.mockRejectedValue(new Error("Server down"));
-
-        renderKeysListWithUserContext({ setUser });
-
-        await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
-        await waitFor(() => expect(screen.queryByTestId("loader")).not.toBeInTheDocument());
-
-        expect(screen.getByTestId("keys-list-error").textContent).toBe("Server down");
-        expect(screen.queryByTestId("keys-account")).not.toBeInTheDocument();
-    });
-
-    test("filters keys based on search input", async () => {
-        getPassKeys.mockResolvedValue([
-            { account: "test1", username: "user1" },
-            { account: "test2", username: "user2" },
-            { account: "prefixedtest1", username: "user3" }
-        ]);
-        renderKeysListWithUserContext({ setUser });
-
-        await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
-        await waitFor(() => expect(screen.queryByTestId("loader")).not.toBeInTheDocument());
-
-        fireEvent.change(screen.getByTestId("search-input"), { target: { value: "test1" } });
-
-        await waitFor(() => {
-            expect(screen.getByText("test1")).toBeInTheDocument();
-            expect(screen.queryByText("prefixedtest1")).toBeInTheDocument();
-            expect(screen.queryByText("test2")).not.toBeInTheDocument();
-        });
-    });
-
-    test("shows empty message when no keys are found", async () => {
-        getPassKeys.mockResolvedValue([
-            { account: "test1", username: "user1" },
-            { account: "test2", username: "user2" },
-        ]);
-        renderKeysListWithUserContext({ setUser });
-
-        await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
-        await waitFor(() => expect(screen.queryByTestId("loader")).not.toBeInTheDocument());
-
-        fireEvent.change(screen.getByTestId("search-input"), { target: { value: "test3" } });
-
-        await waitFor(() => {
-            let noKeysElement = screen.getByTestId("keys-list-no-keys");
-            expect(noKeysElement).toBeInTheDocument();
-            expect(noKeysElement.textContent).toBe("No keys found");
-        });
-    });
-
-    //   test("navigates to add-key page on clicking Add button", async () => {
-    //     renderKeysListWithUserContext({ setUser }, { setEditItem: jest.fn() });
-
-    //     await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
-
-    //     fireEvent.click(screen.getByText(/Add/i));
-    //     await waitFor(() => {
-    //       expect(screen.getByText(/SecureKey/)).toBeInTheDocument(); // Verifies the Navbar is still rendered
-    //       expect(screen.queryByText(/Accounts/i)).not.toBeInTheDocument(); // Ensures we navigated away
-    //     });
-    //   });
-
-    //   test("calls deletePassKey and updates keys list on delete", async () => {
-    //     renderKeysListWithUserContext({ setUser }, { setEditItem: jest.fn() });
-
-    //     await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(1));
-
-    //     fireEvent.click(screen.getAllByText(/Delete/i)[0]);
-
-    //     expect(deletePassKey).toHaveBeenCalledTimes(1);
-    //     expect(deletePassKey).toHaveBeenCalledWith({ userContext: { setUser }, account: "test1", username: "user1" });
-
-    //     await waitFor(() => expect(getPassKeys).toHaveBeenCalledTimes(2)); // fetchPassKeys should be called again after deletion
-    //   });
+  });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -5,7 +5,7 @@
 import "@testing-library/jest-dom";
 
 // src/setupTests.js
-import { TextEncoder, TextDecoder } from 'text-encoding';
+import { TextEncoder, TextDecoder } from "text-encoding";
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
