ca0ca0ac1042832f9958f4f4da2bf21d67fdf65c
Shubham Panchal <panchalshubham0608@gmail.com>
2024-09-07 18:14:46 +0530

    fix accidental commits

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -126,12 +126,6 @@ export default function AuthForm(props) {
         } else {
           setError("An error occurred. Please try again later");
         }
-        let userForContext = createUserForContext({
-          username: credentials.email,
-          password: credentials.password
-        });
-        userContext.setUser(userForContext);
-        setNavigate(<Navigate to="/" />);
       }).finally(() => {
         setLoading(false);
       });
